7c697990b7a36b908a435049087fdd0d7f0f6d75
El-HobbitCR <jefh15@hotmail.com>
2022-05-20 00:23:44 -0600

    Creo el proyecto sin archivos basura, ademas de agregar un componente personalizado.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// importo el componente
+import Parrafo from "./components/Parrafo";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hola Mundo React</h1>
+      {/* Pinto el componente */}
+      <Parrafo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Parrafo.jsx
@@ -0,0 +1,12 @@
+// rafce
+import React from 'react'
+
+const Parrafo = () => {
+    return (
+        <div>
+            <p>Bienvenidos a mi mundo web</p>
+        </div>
+    )
+}
+
+export default Parrafo
