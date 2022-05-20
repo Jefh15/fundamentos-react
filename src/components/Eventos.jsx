e8b174ac670b043eeb92d1e6cfd5a1e48b9c2d95
El-HobbitCR <jefh15@hotmail.com>
2022-05-20 01:10:55 -0600

    Uso de useState() este hook que permite cambiar los estados de mis componentes

--- src/components/Eventos.jsx
+++ src/components/Eventos.jsx
@@ -1,12 +1,20 @@
 // rafce
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
 const Eventos = () => {
+
+    // me creo un hook se estado
+    // texto ---> estado que va a contener nuestro string mi nombre de estado
+    // setTexto --> estado que va a modificar ese texto en cuestion
+    // useState() ---> entre los () va el valor que queremos que tenga po defecto
+    const [texto, setTexto] = useState('Texto desde estado')
 
     // anfn
     // funcion para poder hacer click
     const eventoClick = () => {
         console.log('Me diste un clic')
+        // Cambio el texto con el setText
+        setTexto('cambiando el texto....')
     }
 
 
@@ -15,7 +23,7 @@ const Eventos = () => {
         // Esto es un fragment para poder meter mas de un componente <Fragment></Fragment>
         <Fragment>
             <hr />
-            <h2>Eventos</h2>
+            <h2>{texto}</h2>
             {/* creo un boton */}
             <button
                 // llamo mi evento clic con una funcion de tipo flecha
